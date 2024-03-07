78a543923910259a1afc0e020f96a54183cfce4e
alumnofp <alumnofp@FPB11.CALASANZSALAMANCA.local>
2024-03-07 11:56:44 +0100

    [ADD] Crear Proyecto Galeria

---
+++ La_Galeria/src/main/java/com/galeria/LaGaleriaApplication.java
@@ -0,0 +1,13 @@
+package com.galeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaGaleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaGaleriaApplication.class, args);
+	}
+
+}
