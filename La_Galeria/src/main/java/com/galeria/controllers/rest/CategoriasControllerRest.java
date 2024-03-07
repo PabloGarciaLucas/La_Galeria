877b10799a6be47064fdef31701c47c398a08e9c
Pablo <pablovtsct@gmail.com>
2024-03-07 13:54:05 +0100

    [ADD] POST categorias

--- La_Galeria/src/main/java/com/galeria/controllers/rest/CategoriasControllerRest.java
+++ La_Galeria/src/main/java/com/galeria/controllers/rest/CategoriasControllerRest.java
@@ -1,14 +1,22 @@
 package com.galeria.controllers.rest;
 
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.galeria.entities.CategoriaEntity;
+
 import com.galeria.repositories.CategoriasRepository;
 
 @RestController
@@ -23,6 +31,31 @@ public class CategoriasControllerRest {
 		Iterable<CategoriaEntity> categorias = categoriaRepository.findAll();
 
 		return categorias;
+	}
+
+	@PostMapping("/categorias")
+	public ResponseEntity insertarCategorias(@RequestBody CategoriaEntity categoria)
+			throws ClassNotFoundException, SQLException, NamingException {
+
+		categoriaRepository.save(categoria);
+
+		return new ResponseEntity<>("Categoria insertada correctamente", HttpStatus.OK);
+	}
+
+	@PutMapping("/categorias")
+	public ResponseEntity actualizarCategorias(@RequestBody CategoriaEntity categoria) {
+		categoriaRepository.save(categoria);
+
+		return new ResponseEntity<>("Categoría actualizada correctamente", HttpStatus.OK);
+	}
+
+	@DeleteMapping("/categorias")
+	public ResponseEntity borrarCategorias(@RequestBody CategoriaEntity categoria) {
+
+		categoria.setActivo(0);
+		categoriaRepository.save(categoria);
+
+		return new ResponseEntity<>("Categoría borrada correctamente", HttpStatus.OK);
 	}
 
 }
