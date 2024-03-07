b2c992c02afe92a6b61952f1c623c0b875364c28
Pablo <pablovtsct@gmail.com>
2024-03-07 13:40:18 +0100

    [ADD] GET categorias Correcto

--- La_Galeria/src/main/java/com/galeria/controllers/rest/CategoriasControllerRest.java
+++ La_Galeria/src/main/java/com/galeria/controllers/rest/CategoriasControllerRest.java
@@ -1,26 +1,28 @@
 package com.galeria.controllers.rest;
 
-import java.sql.SQLException;
-import java.util.List;
-
-import javax.naming.NamingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.galeria.entities.CategoriaEntity;
+import com.galeria.repositories.CategoriasRepository;
 
 @RestController
 @RequestMapping("/v1")
 public class CategoriasControllerRest {
-	
+
+	@Autowired
+	CategoriasRepository categoriaRepository;
+
+	@GetMapping("/categorias")
+	public Iterable<CategoriaEntity> obtenerTodasCategorias() {
+		Iterable<CategoriaEntity> categorias = categoriaRepository.findAll();
+
+		return categorias;
+	}
 
 }
--- La_Galeria/src/main/java/com/galeria/entities/CategoriaEntity.java
+++ La_Galeria/src/main/java/com/galeria/entities/CategoriaEntity.java
@@ -31,10 +31,10 @@ public class CategoriaEntity {
     @Column(name = "Activo")
     private Integer activo;
 
-    @OneToMany(mappedBy = "categoria")
-    @JsonIgnore
-    private Set<ProductoEntity> productos;
-    
+//    @OneToMany(mappedBy = "categoria")
+//    @JsonIgnore
+//   private Set<ProductoEntity> productos;
+//    
 
 	public CategoriaEntity() {
 		super();
@@ -45,15 +45,7 @@ public class CategoriaEntity {
 		this.id = id;
 	}
 
-	public CategoriaEntity(String id, String nombre, String descripcion, Integer activo,
-			Set<ProductoEntity> productos) {
-		super();
-		this.id = id;
-		this.nombre = nombre;
-		this.descripcion = descripcion;
-		this.activo = activo;
-		this.productos = productos;
-	}
+	
 	
 
 	public CategoriaEntity(String id, String nombre) {
@@ -110,11 +102,11 @@ public class CategoriaEntity {
 		this.activo = activo;
 	}
 
-	public Set<ProductoEntity> getProductos() {
-		return productos;
-	}
-
-	public void setProductos(Set<ProductoEntity> productos) {
-		this.productos = productos;
-	}
+//	public Set<ProductoEntity> getProductos() {
+//		return productos;
+//	}
+//
+//	public void setProductos(Set<ProductoEntity> productos) {
+//		this.productos = productos;
+//	}
 }
