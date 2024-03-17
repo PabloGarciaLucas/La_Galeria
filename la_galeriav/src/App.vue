7954575681b7f9032783126919416cd7041eefb9
PabloGarciaLucas <pablo.garluc@educa.jcyl.es>
2024-03-17 13:49:45 +0100

    [ADD] Creación de los componentes y vistas de la parte frontend y definición de las rutas

--- la_galeriav/src/App.vue
+++ la_galeriav/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Inicio</router-link> |
+    <router-link to="/solicitarProducto">Solcitar un Producto</router-link>
+    <router-link to="/dudas">Dudas</router-link> |
+    <router-link to="/login">Log In</router-link>
   </nav>
   <router-view/>
 </template>
--- la_galeriav/src/router/index.js
+++ la_galeriav/src/router/index.js
@@ -4,16 +4,33 @@ import HomeView from '../views/HomeView.vue'
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'inicio',
+    component: Inicio
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/solicitarProducto',
+    name: 'solicitarProducto',
+    component: SolicitarProducto
+  },
+  {
+    path: '/dudas',
+    name: 'dudas',
+    component: Dudas
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LogIn
+  },
+  {
+    path: '/solicitarCoctel',
+    name: 'solicitarCoctel',
+    component: SolicitarCoctel
+  },
+  {
+    path: '/personalizarCoctel',
+    name: 'personalizarCoctel',
+    component: PersonalizarCoctel
   }
 ]
 
