a13a452149b1abea48e16c035433865ba81093b8
PabloGarciaLucas <pablo.garluc@educa.jcyl.es>
2024-04-07 13:04:31 +0200

    [ADD] Imports necesarios para FontAwesome en main.js pero hay que hacer npm installs

--- la_galeriav/src/main.js
+++ la_galeriav/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+//import { library } from '@fontawesome/fontawesome-svg-core'
+//import { FontAwesomeIcon} from '@fontawesome/vue-fontawesome'
+//import { faFacebook, faTwitter, faInstagram} from '@fontawesome/free-brands-svg-icons'
 
+//library.add(faFacebook,faTwitter,faInstagram);
+
+//createApp(App).use(router).component('font-awesome-icon',FontAwesomeIcon).mount('#app')
 createApp(App).use(router).mount('#app')
