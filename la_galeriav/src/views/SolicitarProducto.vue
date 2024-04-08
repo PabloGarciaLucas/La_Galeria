9652a16df30dca43c75a87c608e2b6cbc65e92f9
PabloGarciaLucas <pablo.garluc@educa.jcyl.es>
2024-04-08 11:16:20 +0200

    [ADD] SolicitarProducto hecho

--- la_galeriav/src/views/SolicitarProducto.vue
+++ la_galeriav/src/views/SolicitarProducto.vue
@@ -1,28 +1,35 @@
 <template>
-    <div id="app">
-        <h1>Esta es la página para Solicitar un Pedido</h1>
+    <div id="solicitarProducto">
+        <div class="caja">
+            <router-link to="/solicitarCoctel"><img src="solicitarCoctel.png" alt="Solicitar Cóctel" class="imagenes"></router-link> 
+        </div>
+        <div class="caja">
+            <router-link to="/personalizarCoctel"><img src="personalizarCoctel.png" alt="Personalizar Cóctel"></router-link> 
+        </div>
     </div>
 </template>
 
 <style>
-    #app {
+    #solicitarProducto {
         display: flex;
         justify-content: center;
         align-items: center;
         height: 100vh;
+        background-color: #F0F0F0
     }
 
-    .center-content {
-        text-align: center;
+    .caja {
+        border: 3px solid lightgray;
+        border-radius: 10px;
+        padding: 20px;
+        max-width: 500px;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
     }
 
-    .center-image {
-        width: 900px;
-        height: 900px;
-    }
-
-    p {
-        font-weight: bolder;
-        font-size: xx-large;
+    .imagenes {
+        align-self: flex-end;
+        max-width: 50px;
     }
 </style>
