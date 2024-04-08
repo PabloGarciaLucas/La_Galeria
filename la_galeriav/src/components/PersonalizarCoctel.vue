db319bcbb225b1301e8657aaf4d7d54a4f3d99a2
PabloGarciaLucas <pablo.garluc@educa.jcyl.es>
2024-04-08 11:09:55 +0200

    [ADD] PersonalizarCoctel parte visual hecha

--- la_galeriav/src/components/PersonalizarCoctel.vue
+++ la_galeriav/src/components/PersonalizarCoctel.vue
@@ -1,28 +1,100 @@
 <template>
-    <div id="app">
-        <h1>Esta es la página de Personalizar tu Cóctel</h1>
+    <div id="cristaleriaContainer">
+        <div v-for="(cristaleria, index) in cristalerias" :key="index" class="caja">
+            <img src="cristaleria.strDrinkThumb" alt:"cristaleria.strDrink" class="cristaleriaImage">
+        </div>
+    </div>
+
+    <hr>
+
+    <div id="coctelContainer">
+        <div v-for="(coctel, index) in cocteles" :key="index" class="caja">
+            <img src="coctel.strDrinkThumb" alt:"coctel.strDrink" class="coctelImage">
+        </div>
+        <button @click="generarFactura">Solicitar Pedido</button>
     </div>
 </template>
 
-<style>
-    #app {
+<style scoped>
+    #cristaleriaContainer {
         display: flex;
         justify-content: center;
         align-items: center;
         height: 100vh;
+        background-color: #F0F0F0;
     }
 
-    .center-content {
-        text-align: center;
+    .caja {
+        border: 1px solid #ccc;
+        border-radius: 10px;
+        padding: 20px;
+        max-width: 400px;
+        margin: 0 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
 
-    .center-image {
-        width: 900px;
-        height: 900px;
+    .cristaleriaImage {
+        max-width: 100px;
     }
 
-    p {
-        font-weight: bolder;
-        font-size: xx-large;
+    #coctelContainer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: #F0F0F0;
+    }
+
+    .caja {
+        border: 1px solid #ccc;
+        border-radius: 10px;
+        padding: 20px;
+        max-width: 400px;
+        margin: 0 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .coctelImage {
+        max-width: 100px;
     }
 </style>
+
+<script>
+import { jsPDF} from 'jspdf'
+
+export default {
+    data() {
+        return {
+            cocteles: [];
+        }
+    },
+    mounted() {
+        this.fetchCocteles(); //Realizar peticion REST
+    },
+    methods: {
+        async fetchCocteles() {
+            try {
+                const respuesta = await fetch(http://localhost:8080/galeria/v1/productos);
+                const data = await.response.json();
+                this.respuesta = data.drinks;
+            } catch (error) {
+                console.error("Se ha producido un error: " + error);
+            }
+        },
+        async generarFactura() {
+            const cristaleriaSeleccionada = this.cristalerias[0];
+            const coctelSeleccionado = this.cocteles[0];
+
+            const doc = new jsPDF();
+            doc.text("Cristalería Solicitada: $(cristaleriaSeleccionada.strDrink)", 20, 20);
+            doc.text("Cóctel Solicitado: $(coctelSeleccionado.strDrink)", 20, 20);
+
+            doc.save(factura.pdf);
+        }
+    },
+}
+</script>
