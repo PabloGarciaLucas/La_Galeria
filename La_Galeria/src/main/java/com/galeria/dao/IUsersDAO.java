fc7cde94204260b947587bfb04b3de0443898554
PabloGarciaLucas <pablo.garluc@educa.jcyl.es>
2024-04-17 09:34:47 +0200

    [ADD] Creado Interfaz del DAO de Usuarios

---
+++ La_Galeria/src/main/java/com/galeria/dao/IUsersDAO.java
@@ -0,0 +1,20 @@
+package com.galeria.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.naming.NamingException;
+
+import com.galeria.dtos.UsersDTO;
+
+public interface IUsersDAO {
+	
+	public List<UsersDTO> buscarUsuarios(Integer userID, String userName, String userPasswd, String userEmail)
+			throws ClassNotFoundException, SQLException, NamingException;
+
+	public Integer insertarUsuarios(String userName, String userPasswd, String userEmail) throws ClassNotFoundException, SQLException, NamingException;
+
+	public Integer actualizarUsuarios(Integer userID, String userName, String userPasswd, String userEmail) throws ClassNotFoundException, SQLException, NamingException;
+	
+	public Integer borrarUsuarios(Integer userID) throws ClassNotFoundException, SQLException, NamingException;
+}
