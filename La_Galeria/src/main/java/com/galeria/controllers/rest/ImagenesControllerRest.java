adc716ba5e478a871a6889786962e875363cbec5
PabloGarciaLucas <pablo.garluc@educa.jcyl.es>
2024-04-23 11:18:20 +0200

    [ADD] Método para buscar imágenes por id

--- La_Galeria/src/main/java/com/galeria/controllers/rest/ImagenesControllerRest.java
+++ La_Galeria/src/main/java/com/galeria/controllers/rest/ImagenesControllerRest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,6 +34,12 @@ public class ImagenesControllerRest {
 		
 		return imagenEntity;
 	}
+	
+	@GetMapping("/imagenes/{id}")
+	public ImagenEntity obtenerImagenPorID(@PathVariable("idImagen") Integer id) {
+		return imagenRepository.findById(id).orElse(null);
+	}
+
 	
 	@PostMapping("/imagenes")
 	public ResponseEntity insertarImagenes(@RequestBody ImagenEntity imagenEntity) throws ClassNotFoundException, SQLException, NamingException {
