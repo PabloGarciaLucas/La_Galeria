ce64ed8042885d4996d5bb10954ec6afe430a051
uvedvarela <pablovtsct@gmail.com>
2024-04-28 17:18:21 +0200

    [ADD] logica sobre Producto acabada

---
+++ La_Galeria/src/main/java/com/galeria/negocio/IProductosService.java
@@ -0,0 +1,19 @@
+package com.galeria.negocio;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.naming.NamingException;
+
+import com.galeria.dtos.ProductosDTO;
+
+public interface IProductosService {
+	
+	public List<ProductosDTO> buscarProductos(String id, String nombre, String descripcion, String precio, String cantidadEnStock)
+			throws ClassNotFoundException, SQLException, NamingException;
+
+	public Integer insertarProductos(String nombre, String descripcion, String precio, String cantidadEnStock) throws ClassNotFoundException, SQLException, NamingException;
+
+	public Integer actualizarProductos(String id, String nombre, String descripcion, String precio, String cantidadEnStock) throws ClassNotFoundException, SQLException, NamingException;
+
+}
--- La_Galeria/src/main/java/com/galeria/negocio/impl/ProductosService.java
+++ La_Galeria/src/main/java/com/galeria/negocio/impl/ProductosService.java
@@ -1,4 +1,4 @@
-package com.galeria.dao.impl;
+package com.galeria.negocio.impl;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -8,44 +8,36 @@ import javax.naming.NamingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.galeria.dao.IPedidosDAO;
 import com.galeria.dao.IProductosDAO;
-import com.galeria.dtos.PedidosDTO;
 import com.galeria.dtos.ProductosDTO;
-import com.galeria.entities.PedidoEntity;
-import com.galeria.entities.ProductoEntity;
-import com.galeria.repositories.ProductoRepository;
+import com.galeria.negocio.IProductosService;
 
-@Component("ProductosDAO")
-
-public class ProductosDAO implements IProductosDAO{
+@Component
+public class ProductosService implements IProductosService{
 	
 	@Autowired
-	ProductoRepository productoRepository;
+	IProductosDAO productosDAO;
 
 	@Override
 	public List<ProductosDTO> buscarProductos(String id, String nombre, String descripcion, String precio,
 			String cantidadEnStock) throws ClassNotFoundException, SQLException, NamingException {
 		// TODO Auto-generated method stub
-		return productoRepository.buscarProductos(id, nombre, descripcion, precio, cantidadEnStock);
+		return productosDAO.buscarProductos(id, nombre, descripcion, precio, cantidadEnStock);
 	}
 
 	@Override
 	public Integer insertarProductos(String nombre, String descripcion, String precio, String cantidadEnStock)
 			throws ClassNotFoundException, SQLException, NamingException {
-
-		ProductoEntity productoEntity = new ProductoEntity(nombre, descripcion, Double.parseDouble(precio), Integer.parseInt(cantidadEnStock));
-		productoRepository.save(productoEntity);
-		
-		return Integer.parseInt(productoEntity.getId());
+		// TODO Auto-generated method stub
+		return productosDAO.insertarProductos(nombre, descripcion, precio, cantidadEnStock);
 	}
 
 	@Override
 	public Integer actualizarProductos(String id, String nombre, String descripcion, String precio,
 			String cantidadEnStock) throws ClassNotFoundException, SQLException, NamingException {
-		ProductoEntity productoEntity = new ProductoEntity(id, nombre, descripcion, Double.parseDouble(precio), Integer.parseInt(cantidadEnStock));
-		productoRepository.save(productoEntity);
-		
-		return Integer.parseInt(productoEntity.getId()) ;
+		// TODO Auto-generated method stub
+		return productosDAO.actualizarProductos(id, nombre, descripcion, precio, cantidadEnStock);
 	}
 
 }
