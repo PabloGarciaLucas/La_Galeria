f84ed69060dffef27bbdffb7df64b91a8b3f52cc
PabloGarciaLucas <pablo.garluc@educa.jcyl.es>
2024-05-08 19:23:16 +0200

    [MOD] Haciendo Dudas con responsive incompleto

--- la_galeriav/src/views/Dudas.vue
+++ la_galeriav/src/views/Dudas.vue
@@ -5,7 +5,7 @@
         <div class="caja">
           <h2 class="tituloPregunta">¿Cómo funciona nuestra aplicación?</h2>
           <p class="respuesta">El funcionamiento de nuestra aplicación es simple. El cliente cuenta con dos opciones: "Solicitar un cóctel", en la que el cliente solicita un cóctel ya predefinido el cual será enviado a su ubicación, y "Personalizar un Cóctel", en la que permitimos a nuestro cliente, además de escoger su cóctel, escoger la cristalería que desea que tenga su vaso además de poder quedársela para que pueda disfrutar de otros cócteles futuros.</p>
-          <div @load="traerImagen()" class="container"></div>
+          <div @load="traerImagen()" class="containerImagen"></div>
         </div>
       </div>
 
@@ -13,7 +13,7 @@
         <div class="caja">
           <h2 class="tituloPregunta">¿Hasta dónde llegan nuestros servicios?</h2>
           <p class="respuesta">Nuestros servicios por ahora solo abarcan toda Salamanca, pero pretendemos extender este servicio a nivel nacional.</p>
-          <div @load="traerImagen2()" class="container"></div>
+          <div @load="traerImagen2()" class="containerImagen"></div>
         </div>
       </div>
 
@@ -21,7 +21,7 @@
         <div class="caja">
           <h2 class="tituloPregunta">¿Hasta qué horario está abierta la entrega de cócteles los fines de semana?</h2>
           <p class="respuesta">Al ser un servicio pensado para previas, el horario de servicio es de 22:30 hasta las 4:30 de viernes a domingo.</p>
-          <div @load="traerImagen3()" class="container"></div>
+          <div @load="traerImagen3()" class="containerImagen"></div>
         </div>
       </div>
     </div>
@@ -38,17 +38,17 @@ export default {
   },
   methods: {
     traerImagen() {
-      fetch("http://localhost:8080/galeria/v1/imagenes/1", {})
+      fetch("http://localhost:8080/galeria/v1/imagenes/7", {})
         .then(response => response.json())
         .then(data => this.anadeImg(data.imagen));
     },
     traerImagen2() {
-      fetch("http://localhost:8080/galeria/v1/imagenes/1", {})
+      fetch("http://localhost:8080/galeria/v1/imagenes/7", {})
         .then(response => response.json())
         .then(data => this.anadeImg(data.imagen));
     },
     traerImagen3() {
-      fetch("http://localhost:8080/galeria/v1/imagenes/1", {})
+      fetch("http://localhost:8080/galeria/v1/imagenes/7", {})
         .then(response => response.json())
         .then(data => this.anadeImg(data.imagen));
     },
@@ -69,7 +69,7 @@ export default {
 <style scoped>
 #containerForo {
   margin-top: 100px;
-  height: 100vh;
+  min-height: 100vh;
   background-color: #f0f0f0;
 }
 
@@ -77,7 +77,7 @@ export default {
   border: 2px solid #ccc;
   border-radius: 10px;
   padding: 20px;
-  height: 350px;
+  min-height: 350px;
 }
 
 .tituloPregunta {
