d8afe172df29f927d85e7d896f0f40825b403597
uvedvarela <pablovtsct@gmail.com>
2024-05-09 16:35:59 +0200

    [ADD]

--- la_galeriav/src/views/SolicitarProducto.vue
+++ la_galeriav/src/views/SolicitarProducto.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="solicitarProducto" class="d-flex justify-content-center align-items-center flex-wrap">
     <div class="row">
+      <!-- Nuevo div para texto informativo -->
+      <div class="col-md-12 mb-3">
+        <div class="informacion-cocteles">
+          <h2>¡Descubre nuestros deliciosos cócteles!</h2>
+          <p>Explora nuestra selección de cócteles y elige tu favorito para disfrutar de una experiencia única.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
       <div class="caja mb-3 col-md-6">
         <router-link to="/solicitarCoctel" class="enlace text-decoration-none">
           <img src="solicitarCoctel.png" class="imagenes img-fluid">
@@ -8,8 +17,6 @@
           <div @load="traerImagenA()" class="container"></div>
         </router-link>
       </div>
-    </div>
-    <div class="row">
       <div class="caja mb-3 col-md-6">
         <router-link to="/personalizarCoctel" class="enlace text-decoration-none">
           <img src="personalizarCoctel.png" class="imagenes img-fluid">
@@ -82,5 +89,20 @@ export default {
 
 .enlace:hover .titulo {
   color: blue;
+}
+
+/* Estilos para el nuevo div de información */
+.informacion-cocteles {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.informacion-cocteles h2 {
+  font-size: 1.5rem;
+  margin-bottom: 10px;
+}
+
+.informacion-cocteles p {
+  font-size: 1rem;
 }
 </style>
