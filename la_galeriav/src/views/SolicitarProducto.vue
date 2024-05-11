dda1594f9c68f1cd02a030a7832ca39c09a93d8f
PabloGarciaLucas <pablo.garluc@educa.jcyl.es>
2024-05-11 16:54:08 +0200

    [MOD] SolicitarProducto perfecto

--- la_galeriav/src/views/SolicitarProducto.vue
+++ la_galeriav/src/views/SolicitarProducto.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="solicitarProducto" class="container">
     <div class="row">
-      <!-- Nuevo div para texto informativo -->
       <div class="col-md-12 mb-3">
         <div class="informacion-cocteles">
           <h2 class="mb-4">¡Descubre nuestros deliciosos cócteles!</h2>
@@ -12,16 +11,16 @@
     <div class="row">
       <div class="caja mb-3 col-md-6">
         <router-link to="/solicitarCoctel" class="enlace text-decoration-none">
-          <img src="solicitarCoctel.png" class="imagenes img-fluid mb-3">
           <h3 class="titulo mb-2">Solicitar Cóctel</h3>
           <p>¡Elige un cóctel de nuestra lista y te lo prepararemos con los mejores ingredientes!</p>
+          <div id="imagenCoctelA"></div>
         </router-link>
       </div>
       <div class="caja mb-3 col-md-6">
         <router-link to="/personalizarCoctel" class="enlace text-decoration-none">
-          <img src="personalizarCoctel.png" class="imagenes img-fluid mb-3">
           <h3 class="titulo mb-2">Personalizar Cóctel</h3>
           <p>¡Crea tu propio cóctel personalizado eligiendo los ingredientes que más te gusten!</p>
+          <div id="imagenCoctelB"></div>
         </router-link>
       </div>
     </div>
@@ -40,17 +39,17 @@ export default {
     traerImagenA() {
       fetch("http://localhost:8080/galeria/v1/imagenes/2", {})
         .then(response => response.json())
-        .then(data => this.anadeImg(data.imagen));
+        .then(data => this.anadeImg(data.imagen, 'imagenCoctelA'));
     },
     traerImagenB() {
       fetch("http://localhost:8080/galeria/v1/imagenes/3", {})
         .then(response => response.json())
-        .then(data => this.anadeImg(data.imagen));
+        .then(data => this.anadeImg(data.imagen, 'imagenCoctelB'));
     },
-    anadeImg(base64) {
+    anadeImg(base64, id) {
       var img = document.createElement('img');
       img.src = 'data:image/jpeg;base64,' + base64;
-      document.querySelector('.container:last-child').appendChild(img);
+      document.getElementById(id).appendChild(img);
     },
   },
   mounted() {
@@ -63,8 +62,8 @@ export default {
 <style scoped>
 #solicitarProducto {
   background-color: #F0F0F0;
-  padding-top: 80px; /* Añade espacio en la parte superior */
-  padding-bottom: 50px; /* Añade espacio en la parte inferior */
+  padding-top: 80px;
+  padding-bottom: 50px;
 }
 
 .caja {
@@ -74,36 +73,25 @@ export default {
   text-align: center;
 }
 
-.imagenes {
-  max-width: 100px;
-  max-height: 100px; /* Tamaño máximo */
-  margin-bottom: 10px;
-}
-
 .titulo {
   font-size: 1.2rem;
-}
-
-.enlace:hover .imagenes {
-  transform: scale(1.1);
 }
 
 .enlace:hover .titulo {
   color: blue;
 }
 
-/* Estilos para el nuevo div de información */
 .informacion-cocteles {
   text-align: center;
-  margin-bottom: 40px; /* Aumenta el espacio en la parte inferior */
+  margin-bottom: 40px;
 }
 
 .informacion-cocteles h2 {
-  font-size: 2.5rem; /* Aumenta el tamaño del título */
-  margin-bottom: 20px; /* Aumenta el espacio entre el título y el párrafo */
+  font-size: 2.5rem;
+  margin-bottom: 20px;
 }
 
 .informacion-cocteles p {
-  font-size: 1.2rem; /* Aumenta el tamaño del párrafo */
+  font-size: 1.2rem;
 }
 </style>
